b7200f1c82f6927b0243327cec6fc81f72d6b84c
SangGeun-Jeong <toproot0324@gmail.com>
2022-07-25 23:29:02 +0900

    AutoLayout practice

---
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by JSG on 2022/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
