d81432a81160e48b43a7cf20b96af5f4b3a8cbec
SangGeun-Jeong <toproot0324@gmail.com>
2022-09-27 20:58:42 +0900

    16. assert & guard

--- Swift 기초문법/Swift basic.playground/Pages/16.Assert_Guard.xcplaygroundpage/Contents.swift
+++ Swift 기초문법/Swift basic.playground/Pages/16.Assert_Guard.xcplaygroundpage/Contents.swift
@@ -1,12 +1,14 @@
 import Foundation
 
 
-// assert 는 디버기 중 조건의 검증을 위해 사용
+// assert 는 디버깅 중 조건의 검증을 위해 사용
 var value = 0
 assert(value == 0)
 
 // value = 2
 // assert(value == 0, "값이 0이 아닙니다.") // Fail -> 메세지와 함께, runtime error
+// __lldb_expr_1/16.Assert_Guard.xcplaygroundpage:9: Assertion failed: 값이 0이 아닙니다.
+
 
 /*
  guard 조건 else {
@@ -17,10 +19,15 @@ assert(value == 0)
 // guard문으로 Optional binding 가능
 // guard문을 활용해 Optional을 조건문 범위 밖에서도 사용할 수 있음.
 
-func guardTest(value: Int?) {
-	guard let value = value else {
-		return
-	}
+//func guardTest(value: Int) {
+//	guard value == 0 else { return }
+//	print("안녕하세요")
+//}
+//
+//guardTest(value: 0)
+
+func guardTest(value: Int?) { // Int? -> Optional Binding
+	guard let value = value else { return }
 	print(value)
 }
 
